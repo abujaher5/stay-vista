5b0f62529eb0cfd1e5b2593b254b7f3f38abe623
abujaher5 <abujaher5120@gmail.com>
2025-04-09 00:33:42 +0600

    deploy to vercel

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8000;
 
 // middleware
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "https://stay-vista-ad05e.web.app"],
   credentials: true,
   optionSuccessStatus: 200,
 };
